0dfcec1558865a05558e9b81165dc5be823072e7
William Talbot <william.talbot97@gmail.com>
2022-06-28 14:02:19 -0700

    navigation_tools: imu graph test

--- navigation_tools/include/navigation_tools/integrate_imu_graph_test.hpp
+++ navigation_tools/include/navigation_tools/integrate_imu_graph_test.hpp
@@ -8,6 +8,20 @@
 #include <ros/ros.h>
 #include <memory>
 
+// TEST
+#include <gtsam/base/types.h>
+#include <gtsam/geometry/Pose3.h>
+#include <gtsam/navigation/CombinedImuFactor.h>
+#include <gtsam/navigation/NavState.h>
+#include <gtsam/nonlinear/ISAM2.h>
+#include <gtsam/nonlinear/NonlinearFactorGraph.h>
+#include <gtsam/nonlinear/Values.h>
+#include <gtsam/slam/BetweenFactor.h>
+#include <gtsam/slam/PriorFactor.h>
+#include <gtsam/inference/Symbol.h>
+using gtsam::symbol_shorthand::X; // Pose3 (x,y,z,r,p,y)
+using gtsam::symbol_shorthand::V; // Vel   (xdot,ydot,zdot)
+using gtsam::symbol_shorthand::B; // Bias  (ax,ay,az,gx,gy,gz)
 
 class IntegrateImu {
 public:
@@ -36,6 +50,14 @@ private:
     boost::shared_ptr<gtsam::PreintegrationCombinedParams> preintegration_params;
     gtsam::imuBias::ConstantBias imu_bias;
     std::unique_ptr<gtsam::PreintegratedCombinedMeasurements> integrator;
+
+    std::unique_ptr<gtsam::PreintegratedCombinedMeasurements> integrator_TEST;
+    gtsam::NavState state_TEST;
+    std::unique_ptr<gtsam::ISAM2> optimiser_TEST;
+    gtsam::NonlinearFactorGraph new_factors;
+    gtsam::Values new_values;
+    gtsam::imuBias::ConstantBias imu_bias_TEST;
+    int key{1};
 
     // State
     ros::Time integration_timestamp;
--- navigation_tools/src/integrate_imu_graph_test.cpp
+++ navigation_tools/src/integrate_imu_graph_test.cpp
@@ -1,4 +1,4 @@
-#include "navigation_tools/integrate_imu.hpp"
+#include "navigation_tools/integrate_imu_graph_test.hpp"
 #include <eigen_ext/covariance.hpp>
 #include <eigen_ext/geometry.hpp>
 #include <eigen_gtsam/eigen_gtsam.hpp>
@@ -84,36 +84,42 @@ void IntegrateImu::integrate(const sensor_msgs::Imu::ConstPtr& msg) {
         const auto& vel = state.velocity();
 
         // Optimiser TEST
-        // static int counter = 0;
-        // if (++counter % 1 == 0) {
-        state_TEST = integrator_TEST->predict(state_TEST, imu_bias_TEST);
-        std::cerr << "Preintegrated Meas Cov:\n" << integrator_TEST->preintMeasCov() << std::endl;
-        std::cerr << "Preintegrated Meas Cov Diag:\n" << integrator_TEST->preintMeasCov().diagonal() << std::endl;
-        new_factors.emplace_shared<gtsam::CombinedImuFactor>(X(key - 1), V(key - 1), X(key), V(key), B(key - 1),
-                B(key), *integrator_TEST);
-        new_values.insert(X(key), state_TEST.pose());
-        new_values.insert(V(key), state_TEST.velocity());
-        new_values.insert(B(key), imu_bias_TEST);
-        auto result = optimiser_TEST->update(new_factors, new_values);
-        std::cerr << "optimiser before/after errors: " << *result.errorBefore << ", " << *result.errorAfter << "\n";
-        integrator_TEST->resetIntegrationAndSetBias(imu_bias_TEST);
-        //integrator_TEST = std::make_unique<gtsam::PreintegratedCombinedMeasurements>(preintegration_params, imu_bias_TEST);
-        new_factors = gtsam::NonlinearFactorGraph();
-        new_values.clear();
-        gtsam::Pose3 pose_TEST = optimiser_TEST->calculateEstimate<gtsam::Pose3>(X(key));
-        Eigen::Quaterniond q = pose_TEST.rotation().toQuaternion();
-        Eigen::Matrix<double, 6, 6> pose_cov_TEST = optimiser_TEST->marginalCovariance(X(key));
-        std::cerr << "pose_TEST:\n" << pose_TEST.translation().vector() << "\n" << q.w() << ", " << q.x() << ", "
-                << q.y() << ", " << q.z() << "\n";
-        std::cerr << "pose_cov_TEST:\n" << pose_cov_TEST << "\n";
-        std::cerr << "pose_cov_TEST diag:\n" << pose_cov_TEST.diagonal() << "\n";
-        gtsam::Velocity3 vel_TEST = optimiser_TEST->calculateEstimate<gtsam::Velocity3>(V(key));
-        gtsam::Velocity3 vel_cov_TEST = optimiser_TEST->marginalCovariance(V(key));
-        std::cerr << "vel_TEST:\n" << vel_TEST << "\n";
-        std::cerr << "vel_cov_TEST:\n" << vel_cov_TEST << "\n";
-        std::cerr << "vel_cov_TEST diag:\n" << vel_cov_TEST.diagonal() << "\n";
-        ++key;
-        // }
+        static int counter = 0;
+        if (++counter % 10 == 0) {
+            std::cerr << "timestamp:" << imu.timestamp << std::endl;
+            state_TEST = integrator_TEST->predict(state_TEST, imu_bias_TEST);
+            std::cerr << "Preintegrated Meas Cov:\n" << integrator_TEST->preintMeasCov() << std::endl;
+            std::cerr << "Preintegrated Meas Cov Diag:\n" << integrator_TEST->preintMeasCov().diagonal() << std::endl;
+            new_factors.emplace_shared<gtsam::CombinedImuFactor>(X(key - 1), V(key - 1), X(key), V(key), B(key - 1),
+                    B(key), *integrator_TEST);
+            new_values.insert(X(key), state_TEST.pose());
+            new_values.insert(V(key), state_TEST.velocity());
+            new_values.insert(B(key), imu_bias_TEST);
+            auto result = optimiser_TEST->update(new_factors, new_values);
+            std::cerr << "optimiser before/after errors: " << *result.errorBefore << ", " << *result.errorAfter << "\n";
+            integrator_TEST->resetIntegrationAndSetBias(imu_bias_TEST);
+            new_factors = gtsam::NonlinearFactorGraph();
+            new_values.clear();
+            gtsam::Pose3 pose_TEST = optimiser_TEST->calculateEstimate<gtsam::Pose3>(X(key));
+            Eigen::Quaterniond q = pose_TEST.rotation().toQuaternion();
+            Eigen::Matrix<double, 6, 6> pose_cov_TEST = optimiser_TEST->marginalCovariance(X(key));
+            std::cerr << "pose_TEST:\n" << pose_TEST.translation().vector() << "\n" << q.w() << ", " << q.x() << ", "
+                    << q.y() << ", " << q.z() << "\n";
+            std::cerr << "pose_cov_TEST:\n" << pose_cov_TEST << "\n";
+            std::cerr << "pose_cov_TEST diag:\n" << pose_cov_TEST.diagonal() << "\n";
+            gtsam::Velocity3 vel_TEST = optimiser_TEST->calculateEstimate<gtsam::Velocity3>(V(key));
+            Eigen::Matrix3d vel_cov_TEST = optimiser_TEST->marginalCovariance(V(key));
+            std::cerr << "vel_TEST:\n" << vel_TEST << "\n";
+            std::cerr << "vel_cov_TEST:\n" << vel_cov_TEST << "\n";
+            std::cerr << "vel_cov_TEST diag:\n" << vel_cov_TEST.diagonal() << "\n";
+            gtsam::imuBias::ConstantBias bias_TEST = optimiser_TEST->calculateEstimate<gtsam::imuBias::ConstantBias>(
+                    B(key));
+            Eigen::Matrix<double, 6, 6> bias_cov_TEST = optimiser_TEST->marginalCovariance(B(key));
+            std::cerr << "bias_TEST:\n" << bias_TEST << "\n";
+            std::cerr << "bias_cov_TEST:\n" << bias_cov_TEST << "\n";
+            std::cerr << "bias_cov_TEST diag:\n" << bias_cov_TEST.diagonal() << "\n";
+            ++key;
+        }
 
         // Covariance
         const Eigen::Matrix<double, 15, 15> integration_covariance = integrator->preintMeasCov();
@@ -156,13 +162,13 @@ void IntegrateImu::integrate(const sensor_msgs::Imu::ConstPtr& msg) {
         state_TEST = initial_state;
         const gtsam::Pose3 initial_pose = gtsam::Pose3{gtsam::Rot3(orientation), position};
         const gtsam::SharedNoiseModel pose_noise = gtsam::noiseModel::Diagonal::Sigmas((gtsam::Vector(6) <<
-                0.1, 0.1, 0.1, 0.1, 0.1, 0.1).finished());
+                0.001, 0.001, 0.001, 0.001, 0.001, 0.001).finished());
         new_factors.emplace_shared<gtsam::PriorFactor<gtsam::Pose3>>(X(0), initial_pose, pose_noise);
         const gtsam::SharedNoiseModel vel_noise = gtsam::noiseModel::Diagonal::Sigmas((gtsam::Vector(3) <<
-                0.1, 0.1, 0.1).finished());
+                0.001, 0.001, 0.001).finished());
         new_factors.emplace_shared<gtsam::PriorFactor<gtsam::Velocity3>>(V(0), linear_velocity, vel_noise);
         const gtsam::SharedNoiseModel bias_noise = gtsam::noiseModel::Diagonal::Sigmas((gtsam::Vector(6) <<
-                0.01, 0.01, 0.01, 0.01, 0.01, 0.01).finished());
+                0.0001, 0.0001, 0.0001, 0.0001, 0.0001, 0.0001).finished());
         new_factors.emplace_shared<gtsam::PriorFactor<gtsam::imuBias::ConstantBias>>(B(0), imu_bias_TEST, bias_noise);
         new_values.insert(X(0), initial_pose);
         new_values.insert(V(0), linear_velocity);
