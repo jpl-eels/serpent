23e79cabb197bbbf48e85080b61aa3c7f572892b
William Talbot <william.talbot97@gmail.com>
2022-07-11 19:53:59 -0700

    plotting: fixed typo

--- plotting/main.m
+++ plotting/main.m
@@ -13,9 +13,9 @@ config.gt.topic = "/snowboard/lo_frontend/odometry";
 config.gt.accelerometer_bias = [0.0, 0.0, 0.0];
 config.gt.gyroscope_bias = [0.0, 0.0, 0.0];
 config.data_dir = join([config.gt.data_dir, "analysis/"], "");
-config.filenames = ["serpent_s2s_3/odometry.bag", ...
-    "serpent_s2s_4/odometry.bag"];
-config.names = ["s2s v3", "s2s v4"];
+config.filenames = ["serpent_s2m_4/odometry.bag", ...
+    "serpent_s2m_bugfix_4/odometry.bag"];
+config.names = ["s2m v4", "s2m v4 bugfix"];
 config.odom_topics = ["/serpent/optimisation/odometry", ...
     "/serpent/optimisation/odometry"];
 
@@ -28,8 +28,8 @@ plot_opts.colours = {[0 0.4470 0.7410], [0.8500 0.3250 0.0980], ...
     [0.4660 0.6740 0.1880], [0.3010 0.7450 0.9330],[0.6350 0.0780 0.1840]};
 plot_opts.save = true;
 plot_opts.filetypes = ["fig", "png"];
-plot_opts.save_dir = join([config.gt.data_dir, "matlab_analyis/", ...
-    "mammoth/", "serpent_s2s_v3_v4_gt_v4/"], "");
+plot_opts.save_dir = join([config.gt.data_dir, "matlab_analysis/", ...
+    "mammoth/", "serpent_s2m_v4_bugfix/"], "");
 plot_opts.close_after_save = false;
 
 data = data_from_rosbags(config);
