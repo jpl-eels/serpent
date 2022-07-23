706c558cf0c79a87d9a6848b5d590dc76de93007
William Talbot <william.talbot97@gmail.com>
2022-07-23 13:48:39 -0700

    serpent: added x coordinate to stereo geometry filter

--- serpent/include/serpent/match_filters.hpp
+++ serpent/include/serpent/match_filters.hpp
@@ -32,7 +32,10 @@ protected:
 };
 
 /**
- * @brief Filter for removing feature matches that contradict stereo camera geometry.
+ * @brief Filter for removing feature matches that contradict stereo camera geometry. There are two conditions that must
+ * pass:
+ *  1. The left x coordinate must be less than the right x coordinate (i.e. features in the left frame are more left).
+ *  2. The y coordinates must be within a vertical pixel threshold of each other.
  * 
  */
 class StereoMatchFilter{
