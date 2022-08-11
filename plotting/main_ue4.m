eac2eca6cad8d657841be3b3cc255a2472337825
William Talbot <william.talbot97@gmail.com>
2022-08-11 08:31:11 -0700

    ue4_missions: launch / config files for running ue4 missions

--- plotting/main_ue4.m
+++ plotting/main_ue4.m
@@ -5,16 +5,16 @@ close all;
 % Configuration
 config = struct;
 config.gt = struct;
-config.gt.data_dir = "/home/william/data/ue4_pedro/";
-config.gt.filename = "replaced_gt_replaced_topic_names_replaced_frame_ids_ue4_icy_surface_0.bag";
-config.gt.topic = "/true_pose_imu_rate";
+config.gt.data_dir = "/home/william/data/simulator_prcp_gazebo/";
+config.gt.filename = "surface_spiral_2.bag";
+config.gt.topic = "/ground_truth";
 config.gt.accelerometer_bias = [0.0, 0.0, 0.0];
 config.gt.gyroscope_bias = [0.0, 0.0, 0.0];
-config.data_dir = join([config.gt.data_dir, "analysis/"], "");
-config.filenames = ["serpent_s2s_1/analysis.bag", ...
-    "serpent_s2m_1/analysis.bag"];
-config.names = ["s2s", "s2m"];
-config.odom_topics = ["/serpent/optimisation/odometry", ...
+config.data_dir = join([config.gt.data_dir, ...
+    "surface_spiral_2_analysis/"], "");
+config.filenames = ["serpent_imu_int_2/odometry.bag", "serpent_imu_opt_2/odometry.bag"];
+config.names = ["imu int", "imu opt"];
+config.odom_topics = ["/integrate_imu/odometry", ...
     "/serpent/optimisation/odometry"];
 
 plot_opts.align_first_pose = true;
@@ -27,8 +27,8 @@ plot_opts.colours = {[0 0.4470 0.7410], [0.8500 0.3250 0.0980], ...
     [0.4660 0.6740 0.1880], [0.3010 0.7450 0.9330],[0.6350 0.0780 0.1840]};
 plot_opts.save = true;
 plot_opts.filetypes = ["fig", "png"];
-plot_opts.save_dir = join([config.gt.data_dir, "plots/", ...
-    "serpent_pc_reg/"], "");
+plot_opts.save_dir = join([config.gt.data_dir, "matlab_analysis/", ...
+    replace(config.gt.filename, ".bag", "/"), "serpent_imu_replot/"], "");
 plot_opts.close_after_save = false;
 
 data = data_from_rosbags(config);
