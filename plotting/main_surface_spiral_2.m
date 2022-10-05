f334b18f9a5fc117eb273526d45952238b94bba5
William Talbot <william.talbot97@gmail.com>
2022-10-05 16:23:39 +0200

    simulator dataset configuration

--- plotting/main_surface_spiral_2.m
+++ plotting/main_surface_spiral_2.m
@@ -5,17 +5,21 @@ close all;
 % Configuration
 config = struct;
 config.gt = struct;
-config.gt.data_dir = "/home/william/data/ue4_pedro/";
-config.gt.filename = "replaced_gt_replaced_topic_names_replaced_frame_ids_ue4_icy_surface_0.bag";
-config.gt.topic = "/true_pose_imu_rate";
+data_dir = join([getenv("HOME"), "/data/"], "");
+data_source = "simulator_prcp_gazebo/";
+data_name = "surface_ellipse_2";
+config.gt.data_dir = join([data_dir, data_source], "");
+config.gt.filename = join([data_name, ".bag"], "");
+config.gt.topic = "/ground_truth";
 config.gt.accelerometer_bias = [0.0, 0.0, 0.0];
 config.gt.gyroscope_bias = [0.0, 0.0, 0.0];
-config.data_dir = join([config.gt.data_dir, "analysis/"], "");
-config.filenames = ["serpent_s2s_1/analysis.bag", ...
-    "serpent_s2m_1/analysis.bag"];
-config.names = ["s2s", "s2m"];
-config.odom_topics = ["/serpent/optimisation/odometry", ...
-    "/serpent/optimisation/odometry"];
+config.data_dir = join([data_dir, "results/", data_source, data_name, ...
+    "/"], "");
+config.filenames = ["s2s_2022-09-23-14-40-23.bag", ...
+    "s2s_imu_2022-09-23-14-42-49.bag"];
+config.names = ["s2s", "s2s imu"];
+config.odom_topics = repmat("/serpent/optimisation/odometry", 1, ...
+    length(config.names));
 
 plot_opts.align_first_pose = true;
 plot_opts.start_from_time_zero = true;
@@ -27,8 +31,8 @@ plot_opts.colours = {[0 0.4470 0.7410], [0.8500 0.3250 0.0980], ...
     [0.4660 0.6740 0.1880], [0.3010 0.7450 0.9330],[0.6350 0.0780 0.1840]};
 plot_opts.save = true;
 plot_opts.filetypes = ["fig", "png"];
-plot_opts.save_dir = join([config.gt.data_dir, "plots/", ...
-    "serpent_pc_reg/"], "");
+plot_opts.save_dir = join([data_dir, "analysis/", data_source, ...
+    data_name, "/"], "");
 plot_opts.close_after_save = false;
 
 data = data_from_rosbags(config);
