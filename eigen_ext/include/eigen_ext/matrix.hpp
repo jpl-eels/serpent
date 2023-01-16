9c7eda34c7a8f411194efaf05a138fc2793d45e1
William Talbot <william.talbot97@gmail.com>
2023-01-16 12:38:46 +0100

    eigen_ext: added linear interpolation for matrices and vectors

--- eigen_ext/include/eigen_ext/matrix.hpp
+++ eigen_ext/include/eigen_ext/matrix.hpp
@@ -9,23 +9,13 @@ namespace eigen_ext {
 
 /**
  * @brief Extract the 3-dim vector of a skew symmetric matrix. Throws an exception if matrix is not skew symmetric.
- * 
- * @tparam Scalar 
- * @param matrix 
- * @return Eigen::Matrix<Scalar, 3, 1> 
+ *
+ * @tparam Scalar
+ * @param matrix
+ * @return Eigen::Matrix<Scalar, 3, 1>
  */
 template<typename Scalar>
 Eigen::Matrix<Scalar, 3, 1> from_skew_symmetric(const typename Eigen::Matrix<Scalar, 3, 3>& matrix);
-
-/**
- * @brief Create the skew symmetric matrix for a 3-dim vector.
- *
- * @tparam Scalar
- * @param v
- * @return Eigen::Matrix<Scalar, 3, 3>
- */
-template<typename Scalar>
-Eigen::Matrix<Scalar, 3, 3> skew_symmetric(const Eigen::Matrix<Scalar, 3, 1>& v);
 
 /**
  * @brief Check if positive definite through Cholesky decomposition. Note that this function only checks the upper
@@ -41,15 +31,42 @@ bool is_positive_definite(const Eigen::EigenBase<Derived>& m);
 
 /**
  * @brief Check if a matrix is symmetric about its diagonal, according to some level of precision.
- * 
- * @tparam Derived 
- * @param m 
- * @param precision 
- * @return true 
- * @return false 
+ *
+ * @tparam Derived
+ * @param m
+ * @param precision
+ * @return true
+ * @return false
  */
 template<typename Derived>
 bool is_symmetric(const Eigen::DenseBase<Derived>& m, const typename Derived::Scalar precision = 0);
+
+/**
+ * @brief Returns the (elementwise) linear interpolation of two matrices (or vectors) as:
+ *  m1 + interp * (m2 - m1) = (1 - interp) * m1 + interp * m2
+ *
+ * Normally this function would be used for interpolation with interp in [0, 1] where 0 corresponds to m1 and 1 to m2.
+ * However extrapolation is also possible with interp outside [0, 1].
+ *
+ * @tparam Derived
+ * @param m1
+ * @param m2
+ * @param interp
+ * @return Derived
+ */
+template<typename Derived>
+Derived linear_interpolate(const Eigen::DenseBase<Derived>& m1, const Eigen::DenseBase<Derived>& m2,
+        const double interp);
+
+/**
+ * @brief Create the skew symmetric matrix for a 3-dim vector.
+ *
+ * @tparam Scalar
+ * @param v
+ * @return Eigen::Matrix<Scalar, 3, 3>
+ */
+template<typename Scalar>
+Eigen::Matrix<Scalar, 3, 3> skew_symmetric(const Eigen::Matrix<Scalar, 3, 1>& v);
 
 /* Implementation */
 
@@ -61,13 +78,6 @@ Eigen::Matrix<Scalar, 3, 1> from_skew_symmetric(const typename Eigen::Matrix<Sca
         throw std::runtime_error(ss.str());
     }
     return Eigen::Matrix<Scalar, 3, 1>{matrix(2, 1), matrix(0, 2), matrix(1, 0)};
-}
-
-template<typename Scalar>
-Eigen::Matrix<Scalar, 3, 3> skew_symmetric(const Eigen::Matrix<Scalar, 3, 1>& v) {
-    Eigen::Matrix<Scalar, 3, 3> m;
-    m << 0, -v[2], v[1], v[2], 0, -v[0], -v[1], v[0], 0;
-    return m;
 }
 
 template<typename Derived>
@@ -86,6 +96,19 @@ bool is_symmetric(const Eigen::DenseBase<Derived>& m, const typename Derived::Sc
         }
     }
     return true;
+}
+
+template<typename Derived>
+Derived linear_interpolate(const Eigen::MatrixBase<Derived>& m1, const Eigen::MatrixBase<Derived>& m2,
+        const double interp) {
+    return (1.0 - interp) * m1.derived() + interp * m2.derived();
+}
+
+template<typename Scalar>
+Eigen::Matrix<Scalar, 3, 3> skew_symmetric(const Eigen::Matrix<Scalar, 3, 1>& v) {
+    Eigen::Matrix<Scalar, 3, 3> m;
+    m << 0, -v[2], v[1], v[2], 0, -v[0], -v[1], v[0], 0;
+    return m;
 }
 
 }
