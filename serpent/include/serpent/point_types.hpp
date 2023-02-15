d886f323b87d1e5e5be3c72b6dd2f9dd34149148
William Talbot <william.talbot97@gmail.com>
2023-02-15 18:51:35 +0100

    serpent: added fast_gicp functions for custom point types

---
+++ serpent/include/serpent/point_types.hpp
@@ -0,0 +1,14 @@
+#ifndef SERPENT_POINT_TYPES_HPP
+#define SERPENT_POINT_TYPES_HPP
+
+#include <fast_gicp/gicp/fast_gicp.hpp>
+#include <fast_gicp/gicp/fast_gicp_st.hpp>
+#include <fast_gicp/gicp/fast_vgicp.hpp>
+#include <fast_gicp/gicp/impl/fast_gicp_impl.hpp>
+#include <fast_gicp/gicp/impl/fast_gicp_st_impl.hpp>
+#include <fast_gicp/gicp/impl/fast_vgicp_impl.hpp>
+#include <fast_gicp/gicp/impl/lsq_registration_impl.hpp>
+#include <fast_gicp/gicp/lsq_registration.hpp>
+#include <pointcloud_tools/point_types.hpp>
+
+#endif
---
+++ serpent/src/point_types.cpp
@@ -0,0 +1,6 @@
+#include "serpent/point_types.hpp"
+
+template class fast_gicp::FastGICPSingleThread<PointNormalCovariance, PointNormalCovariance>;
+template class fast_gicp::FastGICP<PointNormalCovariance, PointNormalCovariance>;
+template class fast_gicp::FastVGICP<PointNormalCovariance, PointNormalCovariance>;
+template class fast_gicp::LsqRegistration<PointNormalCovariance, PointNormalCovariance>;
