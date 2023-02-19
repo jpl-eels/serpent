328c37dee999f5ee5eaa8b7081beea05b88144e2
William Talbot <william.talbot97@gmail.com>
2023-02-20 00:28:23 +0100

    serpent: added fast_gicp support for new point types

--- serpent/src/point_types.cpp
+++ serpent/src/point_types.cpp
@@ -1,5 +1,9 @@
 #include "serpent/point_types.hpp"
 
+template class fast_gicp::FastGICPSingleThread<PointNormalUnit, PointNormalUnit>;
+template class fast_gicp::FastGICP<PointNormalUnit, PointNormalUnit>;
+template class fast_gicp::FastVGICP<PointNormalUnit, PointNormalUnit>;
+template class fast_gicp::LsqRegistration<PointNormalUnit, PointNormalUnit>;
 template class fast_gicp::FastGICPSingleThread<PointNormalCovariance, PointNormalCovariance>;
 template class fast_gicp::FastGICP<PointNormalCovariance, PointNormalCovariance>;
 template class fast_gicp::FastVGICP<PointNormalCovariance, PointNormalCovariance>;
