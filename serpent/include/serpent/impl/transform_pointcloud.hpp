e86435202bd8984f56bf0f4be4c7ead830cf56b2
William Talbot <william.talbot97@gmail.com>
2023-02-20 00:27:14 +0100

    serpent: added transform functions for new point types

--- serpent/include/serpent/impl/transform_pointcloud.hpp
+++ serpent/include/serpent/impl/transform_pointcloud.hpp
@@ -15,6 +15,18 @@ void transform_pointcloud(const pcl::PointCloud<PointT>& cloud_in, pcl::PointClo
 }
 
 template<typename Scalar>
+void transform_pointcloud(const pcl::PointCloud<PointNormalUnit>& cloud_in, pcl::PointCloud<PointNormalUnit>& cloud_out,
+        const Eigen::Matrix<Scalar, 4, 4>& transform) {
+    pct::transform_pointcloud_with_normals_and_unit_vectors(cloud_in, cloud_out, transform);
+}
+
+template<typename Scalar, int Mode>
+void transform_pointcloud(const pcl::PointCloud<PointNormalUnit>& cloud_in, pcl::PointCloud<PointNormalUnit>& cloud_out,
+        const Eigen::Transform<Scalar, 3, Mode>& transform) {
+    pct::transform_pointcloud_with_normals_and_unit_vectors(cloud_in, cloud_out, transform);
+}
+
+template<typename Scalar>
 void transform_pointcloud(const pcl::PointCloud<PointNormalCovariance>& cloud_in,
         pcl::PointCloud<PointNormalCovariance>& cloud_out, const Eigen::Matrix<Scalar, 4, 4>& transform) {
     pct::transform_pointcloud_with_normals_and_covariances(cloud_in, cloud_out, transform);
--- serpent/include/serpent/transform_pointcloud.hpp
+++ serpent/include/serpent/transform_pointcloud.hpp
@@ -14,6 +14,14 @@ void transform_pointcloud(const pcl::PointCloud<PointT>& cloud_in, pcl::PointClo
         const Eigen::Transform<Scalar, 3, Mode>& transform);
 
 template<typename Scalar>
+void transform_pointcloud(const pcl::PointCloud<PointNormalUnit>& cloud_in, pcl::PointCloud<PointNormalUnit>& cloud_out,
+        const Eigen::Matrix<Scalar, 4, 4>& transform);
+
+template<typename Scalar, int Mode>
+void transform_pointcloud(const pcl::PointCloud<PointNormalUnit>& cloud_in, pcl::PointCloud<PointNormalUnit>& cloud_out,
+        const Eigen::Transform<Scalar, 3, Mode>& transform);
+
+template<typename Scalar>
 void transform_pointcloud(const pcl::PointCloud<PointNormalCovariance>& cloud_in,
         pcl::PointCloud<PointNormalCovariance>& cloud_out, const Eigen::Matrix<Scalar, 4, 4>& transform);
 
